f8a32fcced4e891f47684d34d01612fbac40a9c9
H_SH <115392310+20222014@users.noreply.github.com>
2025-02-28 07:20:16 +0900

    Add files via upload

---
+++ MFCApplication2/MFCApplication2/ImageDialog.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// ImageDialog 대화 상자
+
+class ImageDialog : public CDialogEx
+{
+	DECLARE_DYNAMIC(ImageDialog)
+
+public:
+	ImageDialog(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~ImageDialog();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IMAGE_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual BOOL OnInitDialog();
+	
+	CStatic m_imageControl; // Image Control 변수
+	void LoadImageFromFile(CString imagePath); // 추가
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnStnClickedPic();
+};
---
+++ MFCApplication2/MFCApplication2/OnBnClickedButtonSelect.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// OnBnClickedButtonSelect 대화 상자
+
+class OnBnClickedButtonSelect : public CDialogEx
+{
+	DECLARE_DYNAMIC(OnBnClickedButtonSelect)
+
+public:
+	OnBnClickedButtonSelect(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~OnBnClickedButtonSelect();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
